f6b2f2b8ede16bdd4028d1a22d05936f58e17eff
Bhupesh Patni <patnibhupesh4@gmail.com>
2026-05-26 01:10:19 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+    "https://local-basket-frontend-wbge.onrender.com"],
     credentials:true,
     methods:['POST','GET']
 }
@@ -31,7 +32,8 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+    "https://local-basket-frontend-wbge.onrender.com"],
     credentials:true
 }))
 app.use(express.json())
