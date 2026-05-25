895c4bcc2a78e13aab80600792ace3ced2490f75
Bhupesh Patni <patnibhupesh4@gmail.com>
2026-05-26 00:37:29 +0530

    done

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Nav from './NaV.JSX'
+import Nav from './Nav.jsx'
 import { useSelector } from 'react-redux'
 import { FaUtensils } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
@@ -9,7 +9,7 @@ function OwnerDashboard() {
   const { myShopData } = useSelector(state => state.owner)
   const navigate = useNavigate()
 
-  
+
   return (
     <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center'>
       <Nav />
@@ -45,7 +45,7 @@ function OwnerDashboard() {
             </div>
           </div>
 
-          {myShopData.items.length==0 && 
+          {myShopData.items.length==0 &&
             <div className='flex justify-center items-center p-4 sm:p-6'>
           <div className='w-full max-w-md bg-white shadow-lg rounded-2xl p-6 border border-gray-100 hover:shadow-xl transition-shadow duration-300'>
             <div className='flex flex-col items-center text-center'>
@@ -66,7 +66,7 @@ function OwnerDashboard() {
                 <OwnerItemCard data={item} key={index}/>
               ))}
               </div>}
-            
+
         </div>}
 
 
