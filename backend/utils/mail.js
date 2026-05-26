d4fff44668d6c76fe334849a6199c10479a24f89
Bhupesh Patni <patnibhupesh4@gmail.com>
2026-05-26 12:49:07 +0530

    mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 dotenv.config()
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
