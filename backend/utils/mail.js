6d396d126c9dc43ad259cf5d35eb9778fe3d9a75
Bhupesh Patni <patnibhupesh4@gmail.com>
2026-05-26 12:46:51 +0530

    mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -4,11 +4,19 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
   service: "Gmail",
   port: 465,
-  secure: true, // true for 465, false for other ports
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
   },
+});
+
+transporter.verify((error, success) => {
+   if (error) {
+      console.log("MAIL ERROR =>", error);
+   } else {
+      console.log("MAIL SERVER READY");
+   }
 });
 
 export const sendOtpMail=async (to,otp) => {
@@ -21,16 +29,18 @@ export const sendOtpMail=async (to,otp) => {
 }
 
 
-export const sendDeliveryOtpMail=async (user,otp) => {
-    await transporter.sendMail({
-        from:process.env.EMAIL,
-        to:user.email,
-        subject:"Delivery OTP",
-        html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
+export const sendDeliveryOtpMail = async (user, otp) => {
 
+    console.log("USER =>", user)
+    console.log("EMAIL =>", process.env.EMAIL)
+
+    await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: user.email,
+        subject: "Delivery OTP",
+        html: `<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
     })
 }
-
 
 console.log("EMAIL:", process.env.EMAIL)
 console.log("PASS:", process.env.PASS)
